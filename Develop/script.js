09105819816281a94e11b93122a15dab6404384f
Sandeep GC <sandeep_gc@hotmail.com>
2021-03-20 21:25:07 +1100

    Added Windows Prompt for password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+window.prompt("How long do you want your password to be?");
+window.confirm("Do you want lowercase characters?");
+window.confirm("Do you want uppercase characters?");
+window.confirm("Do you want numeric characters?");
+window.confirm("Do you want special characters?");
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
