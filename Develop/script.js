d260c8ef683174848039b7b55a4004c2e18df8bd
Sandeep GC <sandeep_gc@hotmail.com>
2021-03-20 20:48:43 +1100

    Test Push

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var chars = [0123456789!"#$%&'()*+,-./:;<=>?@[]^_`{|}~ "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwzyz];
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var length = window.prompt("You password must be at least 8 characters and no more than 128 characters");
 
   passwordText.value = password;
 
@@ -15,5 +12,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
