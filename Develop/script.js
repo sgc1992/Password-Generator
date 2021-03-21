a487d309e6117a52b1ae82023d1f2ab1f833ab1b
Sandeep GC <sandeep_gc@hotmail.com>
2021-03-21 12:27:51 +1100

    added if conditions for click event

--- Develop/script.js
+++ Develop/script.js
@@ -2,11 +2,23 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
-window.prompt("How long do you want your password to be?");
-window.confirm("Do you want lowercase characters?");
-window.confirm("Do you want uppercase characters?");
-window.confirm("Do you want numeric characters?");
-window.confirm("Do you want special characters?");
+  var passLength = window.prompt("How long do you want your password to be?");
+  //now passLength will carry the string of whatever the user typed into the prompt box.
+  
+
+  var lowerChars = window.confirm("Do you want lowercase characters?");
+  //now lowerChars will have the value of true or false!
+
+  if(lowerChars){ //if the user answered Okay, lowerChars will be true
+    console.log("You want lower case letters.")
+  } else{
+    console.log("You do NOT want lower case letters.")
+  }
+
+  //these 3 just ask for information, but don't store the value anywhere.
+  window.confirm("Do you want uppercase characters?");
+  window.confirm("Do you want numeric characters?");
+  window.confirm("Do you want special characters?");
 }
 
 
@@ -23,3 +35,4 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+if (passLength > 8) ||  (passLength < 128 ) (passLength = '')
